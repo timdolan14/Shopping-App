fbe74d4e98e414762ab1e7ed0b18b526d8fac309
Tim Dolan <timmydolan14@gmail.com>
2023-08-31 16:10:08 -0400

    Updated Title

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                <h1 className='mt-3'>Shopping App</h1>
+                <h1 className='mt-3'>StyleTrack</h1>
                 <div className='row mt-3'> 
                     <div className='col-sm'>
                         <CartValue />
